e618448f49ad0941c6fb00318635f1ea423814db
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2016-04-18 22:26:40 -0500

    Generation of platforms
    Now it can instantiate new platforms

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetween;
+
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+		//Asignacion del tamaño de las plataformas
+		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(transform.position.x < generationPoint.position.x) {
+
+			//Sigue la posicion y genera las nuevas plataformas
+			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+			//Crear un objeto que ya existe
+			Instantiate (thePlatform, transform.position, transform.rotation);
+			
+		}
+	}
+}

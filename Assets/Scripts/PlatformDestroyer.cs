b07a2b21758d3ea4be1c6427000025340368a457
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2016-04-19 22:13:04 -0500

    Detroying and random platforms
    Now it can destroy all the platform behind the camara and  has been adde
    a prefab so the objects never go missing. The last time was added a
    material

---
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	public GameObject platformDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+
+		//Encuentra el gameobject que tenga ese nombre
+		platformDestructionPoint = GameObject.Find ("PlatformDestructionPoint");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Destruccion de la plataformas
+		if(transform.position.x < platformDestructionPoint.transform.position.x){
+
+			Destroy (gameObject);
+		}
+	
+	}
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -9,6 +9,9 @@ public class PlatformGenerator : MonoBehaviour {
 
 	private float platformWidth;
 
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+
 	// Use this for initialization
 	void Start () {
 		//Asignacion del tamaño de las plataformas
@@ -20,6 +23,8 @@ public class PlatformGenerator : MonoBehaviour {
 	void Update () {
 
 		if(transform.position.x < generationPoint.position.x) {
+
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
 
 			//Sigue la posicion y genera las nuevas plataformas
 			transform.position = new Vector3 (transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);

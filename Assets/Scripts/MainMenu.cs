8d47ef4018b17fe74c2e8f39c60170b4709d5275
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2016-06-21 20:49:23 -0500

    Main Menu
    Has been added a main menu to the game in a new scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	//String que tendra el nombre del nombre del nivel
+	public string playGameLevel;
+
+	//Abre otra escena
+	public void playGame(){
+
+		Application.LoadLevel (playGameLevel);
+	}
+
+	//Cerrar el juego
+	public void quitGame(){
+
+		Application.Quit();		
+	}
+}

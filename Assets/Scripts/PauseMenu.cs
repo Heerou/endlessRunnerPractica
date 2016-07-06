b2241637727937379eeeb1bae405fa5ff6e0397e
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2016-07-05 20:08:35 -0500

    Pause Menu
    Has been added a pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public string mainMenuLevel;
+
+	public GameObject pauseMenu;
+
+	//Pausar juego
+	public void pauseGame(){
+
+		//Detiene el tiempo
+		Time.timeScale = 0f;
+		pauseMenu.SetActive(true);
+	}
+
+	//resumir juego
+	public void resumeGame(){
+
+		Time.timeScale = 1f;
+		pauseMenu.SetActive(false);
+	}
+
+	//Funcion asociada al boton para reiniciar el juego, hace referencia a la funcion Reset
+	public void restartGame(){
+
+		Time.timeScale = 1f;
+		pauseMenu.SetActive(false);
+		FindObjectOfType<GameManager> ().Reset ();
+	}
+
+	//Sale al menu principal
+	//Cuando se carga un nivel primero se debe colocar el tiempo como estaba sino, el tiempo se quedara congelado
+	public void quitToMainMenu(){
+
+		Time.timeScale = 1f;
+		Application.LoadLevel (mainMenuLevel);
+	}
+}

5dc546137f58338e9daa822f14c6970b59157889
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2016-07-10 09:58:08 -0500

    Creating Powerups

---
+++ Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpManager : MonoBehaviour {
+
+	//PowerUps
+	private bool doublePoints;
+	private bool safeMode;
+
+	private bool powerUpActive;
+
+	private float powerUpLenghtCounter;
+
+	private ScoreManager theScoreManager;
+	private PlatformGenerator thePlatformGenerator;
+
+	private float normalPointsPerSecond;
+	private float spikeRate;
+
+	// Use this for initialization
+	void Start () {
+
+		theScoreManager = FindObjectOfType<ScoreManager> ();
+		thePlatformGenerator = FindObjectOfType<PlatformGenerator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(powerUpActive){
+
+			powerUpLenghtCounter -= Time.deltaTime;
+
+			if(doublePoints){
+
+				theScoreManager.pointPerSecond = normalPointsPerSecond * 2;
+				theScoreManager.shouldDouble = true;
+			}
+
+			if(safeMode){
+
+				thePlatformGenerator.randomSpikeThreshold = 0;
+			}
+
+			if(powerUpLenghtCounter <= 0){
+
+				theScoreManager.pointPerSecond = normalPointsPerSecond;
+				theScoreManager.shouldDouble = false;
+				thePlatformGenerator.randomSpikeThreshold = spikeRate;
+
+				powerUpActive = false;
+			}
+		}
+	}
+
+	//Metodo para activar el powerUp
+	public void ActivatePowerUp(bool points, bool safe, float time){
+
+		doublePoints = points;
+		safeMode = safe;
+		powerUpLenghtCounter = time;
+
+		normalPointsPerSecond = theScoreManager.pointPerSecond;
+		spikeRate = thePlatformGenerator.randomSpikeThreshold;
+
+		powerUpActive = true;
+	}
+}
---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUps : MonoBehaviour {
+
+	//PowerUps
+	public bool doublePoints;
+	public bool safeMode;
+
+	//Tiempo del power up
+	public float powerUpLenght;
+
+
+	private PowerUpManager thePowerUpManager;
+	// Use this for initialization
+	void Start () {
+
+		thePowerUpManager = FindObjectOfType<PowerUpManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.name == "Player") {
+
+			thePowerUpManager.ActivatePowerUp (doublePoints, safeMode, powerUpLenght);
+		}
+
+		gameObject.SetActive (false);
+	}
+}
